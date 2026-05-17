14358ee3a5159b10b6e0dd115ea92fc390707e85
qiankong <qiaoxiong9626@gmail.com>
2026-05-17 17:49:39 +0800

    添加哔哩哔哩Cookie的注入

--- libs/cookies/bilibili.js
+++ libs/cookies/bilibili.js
@@ -7,7 +7,7 @@
 // Cookies, look for "DedeUserID". Or visit:
 //   https://api.bilibili.com/x/web-interface/nav
 //
-// 要查找您的 UID：登录 bilibili.com，打开开发者工具 → 应用程序 → Cookies，查找“DedeUserID”。或者访问：
+// 要查找您的 UID：登录 bilibili.com，打开开发者工具 → 应用程序 → Cookies，查找“DedeUserID”。或者访问：  找 mid
 //   https://api.bilibili.com/x/web-interface/nav
 //
 // Replace 12345678 below with your real UID. Add more entries if you
